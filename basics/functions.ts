ea529b110aad2afb039c4287343862293a32fbf3
Aytaditya <adityaaryan531@gmail.com>
2024-05-24 21:49:50 +0530

    functions basics

---
+++ basics/functions.ts
@@ -0,0 +1,12 @@
+function add(a:number,b:number){
+    return a+b;
+}
+console.log(add(4,5))
+
+
+const adi=(inp:string)=>{
+    console.log("Hello Aditya ",inp)
+}
+adi("Aryan")
+
+export {}
--- basics/intro.ts
+++ basics/intro.ts
@@ -2,5 +2,7 @@ let a:number=65
 let adi:string="Aditya"
 let b:number=5.69 //float is also declared as number no special declaration as int, float etc like java or c++
 
+let authToken:boolean=true;
+
 console.log(adi)
 
