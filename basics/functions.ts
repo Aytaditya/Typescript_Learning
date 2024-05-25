0c79833664f0a2e6c5a9149914498b0e91c13f3b
Aytaditya <adityaaryan531@gmail.com>
2024-05-25 17:52:06 +0530

    functions added

--- basics/functions.ts
+++ basics/functions.ts
@@ -9,4 +9,33 @@ const adi=(inp:string)=>{
 }
 adi("Aryan")
 
+
+//correct way to declare function
+function multiply(a:number,b:number):number{
+    return a*b;
+}
+console.log(multiply(4,5))
+
+const rtn=(s:string):string=>{
+    return "I am Batman"
+}
+
+const heros=['Batman','Superman','Flash']
+
+heros.map((hero):string=>{
+   return  `Hero is ${hero}`
+})
+
+//void when function does not return anything
+function hii(name:string):void{
+    console.log("Hello ",name)
+}
+hii("Aditya")
+
+//never when function does not return anything and throws an error
+function handleError(msg:string):never{
+    throw new Error(msg)
+}
+
+
 export {}
